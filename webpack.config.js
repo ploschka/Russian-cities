cad5f47c17b99a3c3395ae6f31e1151771f879b4
Ploshka <folga12381@gmail.com>
2023-01-17 19:04:19 +1000

    Сайт готов

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,22 @@ Encore
     .setPublicPath('/build/')
     .addEntry('index', './src/templates/pages/index.pug')
     .addEntry('table', './src/templates/pages/table.pug')
-    .addEntry('moscow', './src/templates/pages/moscow.pug')
-    // .splitEntryChunks()
+
+    .addEntry('chelyabinsk', './src/templates/pages/cities/chelyabinsk.pug')
+    .addEntry('ekaterinburg', './src/templates/pages/cities/ekaterinburg.pug')
+    .addEntry('kazan', './src/templates/pages/cities/kazan.pug')
+    .addEntry('khabarovsk', './src/templates/pages/cities/khabarovsk.pug')
+    .addEntry('moscow', './src/templates/pages/cities/moscow.pug')
+    .addEntry('nizhni-novgorod', './src/templates/pages/cities/nizhni-novgorod.pug')
+    .addEntry('novosibirsk', './src/templates/pages/cities/novosibirsk.pug')
+    .addEntry('piter', './src/templates/pages/cities/piter.pug')
+    .addEntry('rostov-na-donu', './src/templates/pages/cities/rostov-na-donu.pug')
+    .addEntry('saratov', './src/templates/pages/cities/saratov.pug')
+    .addEntry('simferopol', './src/templates/pages/cities/simferopol.pug')
+    .addEntry('sochi', './src/templates/pages/cities/sochi.pug')
+    .addEntry('syzran', './src/templates/pages/cities/syzran.pug')
+    .addEntry('voronezh', './src/templates/pages/cities/voronezh.pug')
+
     .cleanupOutputBeforeBuild()
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
@@ -22,7 +36,6 @@ Encore
         config.corejs = 3;
     })
     .enableSingleRuntimeChunk()
-    // .enableSassLoader()
     .addPlugin(new PugPlugin())
     .addLoader({test: /\.pug/, loader: PugPlugin.loader})
     .addLoader({test: /\.(css|sass|scss)$/, use: ['css-loader', 'sass-loader']})
